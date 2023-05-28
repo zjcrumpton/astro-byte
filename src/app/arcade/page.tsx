ce845da4d7b2e01358942104010c4e6503e148ce
root <root@DESKTOP-3P0UDF7>
2023-05-27 20:26:07 -0600

    create placeholder COMING SOON page for arcade

--- src/app/arcade/page.tsx
+++ src/app/arcade/page.tsx
@@ -1,7 +1,9 @@
 export default function Arcade() {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1 className="text-3xl">COMING SOON</h1>
+      </main>
     </>
   );
 }
